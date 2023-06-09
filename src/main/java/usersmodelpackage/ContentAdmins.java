7918da1284cca06aec90fbffa147345302837f7a
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2023-06-09 13:23:55 +0300

    Fixed the .xml file to automatically launch the LoginPage.jsp whem running Tomcat

--- src/main/java/usersmodelpackage/ContentAdmins.java
+++ src/main/java/usersmodelpackage/ContentAdmins.java
@@ -20,7 +20,6 @@ public class ContentAdmins extends Users{
         //Uses the DbHelper class to insert the film into the db, displays the appropriate message and returns the film that was inserted.
         if (DbHelper.addNewFilm(filmToBeInserted)) {
             System.out.println("Film with title " + filmToBeInserted.getFilmTitle() + " inserted successfully by " + username);
-            //!!! Σε αυτό, όπως και στα υπόλοιπα (και σε όλες τις κλάσεις), να βρούμε τι θα κάνουμε με το System.out...
         } else {
             System.out.println("Film failed to insert ");
         }
@@ -33,7 +32,7 @@ public class ContentAdmins extends Users{
     }
 
     public void createNewProvoli(int filmId, int cinemaId, LocalDateTime startTime) throws SQLException {
-        // 1 -> Get the cinema and film objects from the db based in their id's using the DbHelper class
+        // 1 -> Get the cinema and film objects from the db based οn their id's using the DbHelper class
         Cinemas cinema = DbHelper.getCinema(cinemaId);
         Films film = DbHelper.getFilm(filmId);
 
